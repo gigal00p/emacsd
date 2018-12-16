5d0e9f6345812217055aadc9afbe5b798675d7d2
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2018-12-16 22:10:04 +0100

    Sort required packages alphabetically

--- custom-setups/packages-setup.el
+++ custom-setups/packages-setup.el
@@ -1,31 +1,31 @@
 (require 'cl)
 (require 'package)
 
-(setq cfg-var:packages '(
-       emmet-mode
-       flycheck
-       monokai-theme
-       rainbow-mode
-       browse-kill-ring
-       ace-jump-mode
-       ace-jump-buffer
-       key-chord
-       helm
-       magit
-       multiple-cursors
-       ace-window ; C-F12 - jump to other frame in emacs
-       move-text ; move text lines up down
-       esup ; startup profiling, M-x esup
-       yasnippet))
+(setq cfg-var:packages
+      '(ace-jump-buffer
+        ace-jump-mode
+        ace-window             ; jump to other frame in emacs
+        browse-kill-ring
+        emmet-mode
+        esup                   ; startup profiling
+        flycheck
+        helm
+        key-chord
+        magit
+        monokai-theme
+        move-text              ; move text lines up down
+        multiple-cursors
+        rainbow-mode
+        yasnippet ))
 
 (defun cfg:install-packages ()
-    (let ((pkgs (remove-if #'package-installed-p cfg-var:packages)))
-        (when pkgs
-            (message "%s" "Emacs refresh packages database...")
-            (package-refresh-contents)
-            (message "%s" " done.")
-            (dolist (p cfg-var:packages)
-                (package-install p)))))
+  (let ((pkgs (remove-if #'package-installed-p cfg-var:packages)))
+    (when pkgs
+      (message "%s" "Emacs refresh packages database...")
+      (package-refresh-contents)
+      (message "%s" " done.")
+      (dolist (p cfg-var:packages)
+        (package-install p)))))
 
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 ;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
