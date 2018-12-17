14a682467a5ca39efa201b906afe2edcd1a9b637
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2018-12-17 11:50:34 +0100

    Switch to Hack font

--- custom-setups/machine-specific-settings.el
+++ custom-setups/machine-specific-settings.el
@@ -23,7 +23,7 @@
 	;; If you edit it by hand, you could mess it up, so be careful.
 	;; Your init file should contain only one such instance.
 	;; If there is more than one, they won't work right.
-	'(default ((t (:family "Consolas" :foundry "unknown" :slant normal :weight normal :height 145 :width normal))))
+	'(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 145 :width normal))))
 	'(hl-line ((t (:underline nil))))))
       )
 
