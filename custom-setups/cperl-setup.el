26e2adb5bca0bb7e604ff514edecb3c82171a638
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2018-12-25 13:27:10 +0100

    Remove unused code from cperl-setup.el

--- custom-setups/cperl-setup.el
+++ custom-setups/cperl-setup.el
@@ -1,19 +1,5 @@
 (require 'perltidy)
 
-;;;;; PLSENSE
-; (require 'plsense)
-; ; ;; Key binding
-; (setq plsense-popup-help-key "C-:")
-; (setq plsense-display-help-buffer-key "M-:")
-; (setq plsense-jump-to-definition-key "C->")
-
-;; Make config suit for you. About the config item, eval the following sexp.
-;; (customize-group "plsense")
-
-;; Do setting recommemded configuration
-;(plsense-config-default)
-;;;;;;; PLSENSE
-
 (defalias 'perl-mode 'cperl-mode)
 
 (add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
@@ -21,10 +7,6 @@
 (custom-set-faces
        '(cperl-array-face ((t (:foreground "indian red" :weight bold))))
        '(cperl-hash-face ((t (:foreground "gold" :slant italic :weight bold)))))
-
-
-; full cperl?
-;(setq cperl-hairy t)
 
 (setq cperl-indent-level 4
       cperl-close-paren-offset -4
@@ -48,22 +30,5 @@
 
 (after 'cperl-mode
   (define-key cperl-mode-map (kbd "<f9>") 'mark-and-run))
-
-; Enable flymake by default in cperl-mode
-;(add-hook 'cperl-mode-hook (lambda ()
-;			     (flycheck-mode t)
-			     ;(flymake-mode t) ;2016-04-16 - disabbled
-			     ;(require 'perl-completion)
-			     ;(perl-completion-mode t)
-			     ;(require 'flymake-cursor) ;2016-04-16 - disabbled
-;			     ))
-
-; (add-hook  'cperl-mode-hook
-;            (lambda ()
-;              (when (require 'auto-complete nil t) ; no error whatever auto-complete.el is not installed.
-;                (auto-complete-mode t)
-;                (make-variable-buffer-local 'ac-sources)
-;                (setq ac-sources
-;                      '(ac-source-perl-completion)))))
 
 (provide 'cperl-setup)
