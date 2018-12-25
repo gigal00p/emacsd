c00421e368825f0246f474386b61e9574930fb7d
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2018-12-25 20:13:10 +0100

    Move bunch of unused elisp to unused directory

--- unused-elisp/go-setup.el
+++ unused-elisp/go-setup.el
@@ -1,6 +1,6 @@
-(require 'go-flymake)             ; compiled binary that must exist in GOPATH
+;(require 'go-flymake)             ; compiled binary that must exist in GOPATH
 
-(require 'quickrun)
+;(require 'quickrun)
 (define-key go-mode-map (kbd "<f9>") 'quickrun)
 (define-key go-mode-map (kbd "M-.") 'godef-jump)         ; Go to definition
 
