f737dff555a5632ed4681b3fc1b92b713d14581b
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2018-12-25 13:29:33 +0100

    New setup file: cl-setup.el

---
+++ custom-setups/cl-setup.el
@@ -0,0 +1,10 @@
+(load "C:/Users/krzysztof/quicklisp/slime-helper.el")
+(setq inferior-lisp-program "C:\\tools\\soft\\sbcl\\sbcl.exe")
+
+(add-to-list 'load-path "C:/tools/soft/sbcl")
+
+(require 'slime)
+
+(slime-setup '(slime-fancy slime-tramp slime-asdf slime-xref-browser slime-highlight-edits slime-sprof slime company))
+
+(provide 'cl-setup)
