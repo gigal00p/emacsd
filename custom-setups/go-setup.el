3537005b70a51ada2141a2f0e75c750e71be64fc
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2018-12-25 13:28:01 +0100

    Remove auto-complete from go-setup.el

--- custom-setups/go-setup.el
+++ custom-setups/go-setup.el
@@ -1,11 +1,4 @@
-(global-auto-complete-mode 1)     ; loads autocomplete globally
-(require 'auto-complete-config)
-(add-hook 'go-mode-hook #'auto-complete-mode 1)
-
-;(require 'go-mode-autoloads)
-(require 'go-autocomplete)        ; server listtening on specified port
 (require 'go-flymake)             ; compiled binary that must exist in GOPATH
-
 
 (require 'quickrun)
 (define-key go-mode-map (kbd "<f9>") 'quickrun)
