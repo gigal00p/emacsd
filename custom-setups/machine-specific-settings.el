266c27ea05560f714c574a9c7b3db3748b73aafb
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-03-06 12:14:08 +0100

    fanless specific settings

--- custom-setups/machine-specific-settings.el
+++ custom-setups/machine-specific-settings.el
@@ -1,5 +1,5 @@
 (cond (
-       (string= (system-name) "DESKTOP-SP63KME" )
+       (string= (system-name) "fanless" )
 
        ;; System locale to use for formatting time values.
        (setq system-time-locale "C")    ; Make sure that the weekdays in the
@@ -23,7 +23,7 @@
 	;; If you edit it by hand, you could mess it up, so be careful.
 	;; Your init file should contain only one such instance.
 	;; If there is more than one, they won't work right.
-	'(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
+	'(default ((t (:family "Monospace" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
 	'(hl-line ((t (:underline nil))))))
       )
 
