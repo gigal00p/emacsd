be4c5a440afa7c985048488927096285dd9d897e
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-03-07 09:11:57 +0100

    Re-enable arrows

--- custom-setups/keybindings.el
+++ custom-setups/keybindings.el
@@ -85,18 +85,18 @@
 
 
 ; This is insane :) Disable completely arrows...
-(global-unset-key (kbd "<left>"))
-(global-unset-key (kbd "<right>"))
-(global-unset-key (kbd "<up>"))
-(global-unset-key (kbd "<down>"))
-(global-unset-key (kbd "<C-left>"))
-(global-unset-key (kbd "<C-right>"))
-(global-unset-key (kbd "<C-up>"))
-(global-unset-key (kbd "<C-down>"))
-(global-unset-key (kbd "<M-left>"))
-(global-unset-key (kbd "<M-right>"))
-(global-unset-key (kbd "<M-up>"))
-(global-unset-key (kbd "<M-down>"))
+; (global-unset-key (kbd "<left>"))
+; (global-unset-key (kbd "<right>"))
+; (global-unset-key (kbd "<up>"))
+; (global-unset-key (kbd "<down>"))
+; (global-unset-key (kbd "<C-left>"))
+; (global-unset-key (kbd "<C-right>"))
+; (global-unset-key (kbd "<C-up>"))
+; (global-unset-key (kbd "<C-down>"))
+; (global-unset-key (kbd "<M-left>"))
+; (global-unset-key (kbd "<M-right>"))
+; (global-unset-key (kbd "<M-up>"))
+; (global-unset-key (kbd "<M-down>"))
 
 ;;keep cursor at same position when scrolling
 (setq scroll-preserve-screen-position 1)
