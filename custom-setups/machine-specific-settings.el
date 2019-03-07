4c332b22be83220812baefce5b8f6f3efed0a901
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-03-07 09:38:32 +0100

    machine-specific-settings.el cleanup

--- custom-setups/machine-specific-settings.el
+++ custom-setups/machine-specific-settings.el
@@ -1,29 +1,23 @@
 (cond (
        (string= (system-name) "fanless" )
-       ;; System locale to use for formatting time values.
-       (setq system-time-locale "C")    ; Make sure that the weekdays in the
-					; time stamps of your Org mode files and
-					; in the agenda appear in English.
-
-       ;(setq org-plantuml-jar-path "C:\\ITools\\ProgramFiles\\japps\\plantuml\\plantuml.jar")
-
+       (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
        (custom-set-variables
-	;; custom-set-variables was added by Custom.
-	;; If you edit it by hand, you could mess it up, so be careful.
-	;; Your init file should contain only one such instance.
-	;; If there is more than one, they won't work right.
 	'(column-number-mode t)
 	'(custom-safe-themes
 	  (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
-	'(smooth-scroll-margin 2)
-	)
+	'(smooth-scroll-margin 2))
        (custom-set-faces
-	;; custom-set-faces was aadded by Custom.
-	;; If you edit it by hand, you could mess it up, so be careful.
-	;; Your init file should contain only one such instance.
-	;; If there is more than one, they won't work right.
 	'(default ((t (:family "Monospace" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
 	'(hl-line ((t (:underline nil))))))
-      )
+      ((string= (system-name) "walkiewk-2EFFT0")
+       (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
+       (custom-set-variables
+	'(column-number-mode t)
+	'(custom-safe-themes
+	  (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
+	'(smooth-scroll-margin 2))
+       (custom-set-faces
+	'(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
+	'(hl-line ((t (:underline nil)))))))
 
 (provide 'machine-specific-settings)
