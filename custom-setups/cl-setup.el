9344f14ae709b3bb7d3f851b503ecbe8949e2a6d
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-03-07 09:36:53 +0100

    Clean cl-setup

--- custom-setups/cl-setup.el
+++ custom-setups/cl-setup.el
@@ -1,16 +1,11 @@
 ;(eval-after-load 'lisp-mode "C:/Users/krzysztof/quicklisp/slime-helper.el")
-
 ;(setq inferior-lisp-program "C:\\tools\\soft\\sbcl\\sbcl.exe")
-
 ;(add-to-list 'load-path "C:/tools/soft/sbcl")
 
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(eval-after-load 'lisp-mode "~/quicklisp/slime-helper.el")
 (setq inferior-lisp-program "sbcl")
-
 (require 'slime)
-
 (slime-setup '(slime-fancy slime-tramp slime-asdf slime-xref-browser slime-highlight-edits slime-sprof slime company))
-
 (load "/home/krzysztof/quicklisp/clhs-use-local.el" t)
 
 (provide 'cl-setup)
