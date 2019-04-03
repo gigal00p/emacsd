09a01da2a3edc1fe3f26cc79a7ee83cc3f546aec
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-04-03 22:23:03 +0200

    Enable paredit in cl

--- custom-setups/cl-setup.el
+++ custom-setups/cl-setup.el
@@ -5,7 +5,43 @@
 (eval-after-load 'lisp-mode "~/quicklisp/slime-helper.el")
 (setq inferior-lisp-program "sbcl")
 (require 'slime)
-(slime-setup '(slime-fancy slime-tramp slime-asdf slime-xref-browser slime-highlight-edits slime-sprof slime company))
-(load "/home/krzysztof/quicklisp/clhs-use-local.el" t)
 
+; (slime-setup '(slime-fancy
+;              slime-fuzzy
+;              slime-fontifying-fu
+;              slime-tramp
+;              slime-asdf
+;              slime-xref-browser
+;              slime-highlight-edits
+;              slime-sprof
+;              slime-scratch
+;              slime
+;              company))
+
+(slime-setup '(slime-asdf
+               slime-autodoc
+               slime-editing-commands
+               slime-fancy
+               slime-fontifying-fu
+               slime-fuzzy
+               slime-indentation
+               slime-mdot-fu
+               slime-package-fu
+               slime-references
+               slime-repl
+               slime-sbcl-exts
+               slime-scratch
+               slime
+               company
+               slime-xref-browser))
+
+(setq slime-complete-symbol*-fancy t
+      slime-completion-at-point-functions 'slime-fuzzy-complete-symbol
+      slime-when-complete-filename-expand t
+      slime-truncate-lines nil
+      slime-autodoc-use-multiline-p t)
+
+(add-hook 'slime-repl-mode-hook #'paredit-mode)
+
+(load "/home/krzysztof/quicklisp/clhs-use-local.el" t)
 (provide 'cl-setup)
