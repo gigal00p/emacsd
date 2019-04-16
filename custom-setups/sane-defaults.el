076e27cb75efc97b88db0cd91ca9dbbceabae827
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-04-16 09:36:52 +0200

    Ediff configuration

--- custom-setups/sane-defaults.el
+++ custom-setups/sane-defaults.el
@@ -127,4 +127,17 @@
 ; open files in same frame
 (setq ns-pop-up-frames nil)
 
+
+;; ediff setup
+(require 'ediff)
+;; don't start another frame
+;; this is done by default in preluse
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+;; put windows side by side
+(setq ediff-split-window-function (quote split-window-horizontally))
+
+;;revert windows on exit - needs winner mode
+(winner-mode)
+(add-hook 'ediff-after-quit-hook-internal 'winner-undo)
+
 (provide 'sane-defaults)
