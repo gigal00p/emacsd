5aa2618c906472eb65dbaf282512fda5bd379c1a
Krzysztof Walkiewicz <kwalkiewicz@egnyte.com>
2019-05-08 20:24:32 +0200

    Setup emacs on dell linux laptop

--- custom-setups/machine-specific-settings.el
+++ custom-setups/machine-specific-settings.el
@@ -30,7 +30,7 @@
        (custom-set-faces
 	'(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
 	'(hl-line ((t (:underline nil))))))
-      ((string= (system-name) "DESKTOP-SP63KME")
+      ((string= (system-name) "dell")
        (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
        (global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
        (custom-set-variables
@@ -39,7 +39,7 @@
 	  (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
 	'(smooth-scroll-margin 2))
        (custom-set-faces
-	'(default ((t (:family "PublicSans-Regular" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
+	'(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
 	'(hl-line ((t (:underline nil)))))))
 
 (provide 'machine-specific-settings)
