97428082d06a9bb0768b3dd8930bb621a3f5e341
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-05-08 20:23:47 +0200

    Golang - configure M-x compile

--- custom-setups/golang-setup.el
+++ custom-setups/golang-setup.el
@@ -1,10 +1,17 @@
-(setenv "GOPATH" "/Users/walkiewk/code/go-workspace")
+;(setenv "GOPATH" "/Users/walkiewk/code/go-workspace")
 
-(add-to-list 'exec-path "/Users/walkiewk/code/go-workspace/bin")
+(setenv "GOPATH" "/home/krzysztof/code/go")
+(add-to-list 'exec-path "/home/krzysztof/code/go/bin")
 
 (defun my-go-mode-hook ()
+  ; Use goimports instead of go-fmt
+  (setq gofmt-command "goimports")
   ; Call Gofmt before saving
   (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
   ; Godef jump key binding
   (local-set-key (kbd "M-.") 'godef-jump)
   (local-set-key (kbd "M-*") 'pop-tag-mark))
@@ -23,3 +30,5 @@
   (require 'go-autocomplete))
 
 (provide 'golang-setup)
+
+
