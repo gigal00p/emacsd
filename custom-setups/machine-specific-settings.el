30d269051bba5c82790fc8a27b0dc9c675737261
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-08-13 16:33:17 +0200

    Eshell alias configuration

--- custom-setups/machine-specific-settings.el
+++ custom-setups/machine-specific-settings.el
@@ -30,7 +30,7 @@
        (custom-set-faces
 	'(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
 	'(hl-line ((t (:underline nil))))))
-      ((string= (system-name) "dell")
+      ((string= (system-name) "DESKTOP-GIPQAG6")
        (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
        (global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
        (custom-set-variables
@@ -39,7 +39,7 @@
 	  (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
 	'(smooth-scroll-margin 2))
        (custom-set-faces
-	'(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
+	'(default ((t (:family "Consolas" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
 	'(hl-line ((t (:underline nil)))))))
 
 (provide 'machine-specific-settings)
