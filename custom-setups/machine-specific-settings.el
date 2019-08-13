2616e2e0226f935e1d81db818f7c05adc6725a85
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-08-13 23:25:03 +0200

    Ubuntu setup

--- custom-setups/machine-specific-settings.el
+++ custom-setups/machine-specific-settings.el
@@ -32,7 +32,8 @@
        (custom-set-faces
 	'(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
 	'(hl-line ((t (:underline nil))))))
-      ((string= (system-name) "DESKTOP-GIPQAG6")
+
+      ((string= (system-name) "latitude-5490")
        (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
        (global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
        (custom-set-variables
@@ -41,7 +42,7 @@
 	  (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
 	'(smooth-scroll-margin 2))
        (custom-set-faces
-	'(default ((t (:family "Consolas" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
+	'(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
 	'(hl-line ((t (:underline nil)))))))
 
 (provide 'machine-specific-settings)
