120867f6c7b429afcf2b15dd41877893ec2b0909
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2019-12-20 10:12:48 +0100

    Comment out rust mode file to silence warning message

--- unused-elisp/rust-setup.el
+++ unused-elisp/rust-setup.el
@@ -1,21 +1,21 @@
-(autoload 'rust-mode "rust-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook #'company-mode)
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-
-(after 'rust-mode
-  (define-key rust-mode-map (kbd "M-a") 'rust-beginning-of-defun)
-  (define-key rust-mode-map (kbd "M-e") 'rust-end-of-defun))
-
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-(setq company-tooltip-align-annotations t)
-
-
-(require 'flycheck)
-(require 'flycheck-rust)
-(require 'electric)
-
-(provide 'rust-setup)
+; (autoload 'rust-mode "rust-mode" nil t)
+; (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+; 
+; (add-hook 'rust-mode-hook #'racer-mode)
+; (add-hook 'racer-mode-hook #'eldoc-mode)
+; (add-hook 'racer-mode-hook #'company-mode)
+; (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+; 
+; (after 'rust-mode
+;   (define-key rust-mode-map (kbd "M-a") 'rust-beginning-of-defun)
+;   (define-key rust-mode-map (kbd "M-e") 'rust-end-of-defun))
+; 
+; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+; (setq company-tooltip-align-annotations t)
+; 
+; 
+; (require 'flycheck)
+; (require 'flycheck-rust)
+; (require 'electric)
+; 
+; (provide 'rust-setup)
