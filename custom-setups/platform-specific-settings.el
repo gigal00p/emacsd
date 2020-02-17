875f1b3cdae0b58ed28be6cd52ad13c1b90caa48
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2020-02-17 18:47:05 +0100

    BSD settings in platform-specific-settings

--- custom-setups/platform-specific-settings.el
+++ custom-setups/platform-specific-settings.el
@@ -34,6 +34,19 @@
    '(smooth-scroll-margin 2))
   (custom-set-faces
    '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
+   '(hl-line ((t (:underline nil))))))
+
+  ((string-equal system-type "berkeley-unix")
+  (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
+  (global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
+  (setq system-time-locale "C")
+  (custom-set-variables
+   '(column-number-mode t)
+   '(custom-safe-themes
+     (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
+   '(smooth-scroll-margin 2))
+  (custom-set-faces
+   '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
    '(hl-line ((t (:underline nil)))))))
 
 
