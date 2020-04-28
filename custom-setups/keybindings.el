0781f9f0cbe2b10605e54838e63d94a975b6a49d
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2020-04-28 20:46:31 +0200

    Alternative Keybinding for set-rectangular-region-anchor

--- custom-setups/keybindings.el
+++ custom-setups/keybindings.el
@@ -139,7 +139,7 @@
 (global-set-key (kbd "C-c k") 'counsel-ag)
 (global-set-key (kbd "C-x l") 'counsel-locate)
 (global-set-key (kbd "C-c h") 'counsel-imenu)
-
+(global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
 (global-set-key (kbd "M-s s") 'isearch-forward)
 
 (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
