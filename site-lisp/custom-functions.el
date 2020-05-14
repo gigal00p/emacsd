1ffd9288bccdb54f2d53a5d5e41ff604dd9197bc
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2020-05-14 12:19:08 +0200

    dired-get-size of marked files

--- site-lisp/custom-functions.el
+++ site-lisp/custom-functions.el
@@ -388,4 +388,14 @@ Version 2015-06-12"
 
 (add-hook 'find-file-hooks 'no-junk-please-were-unixish)
 
+(defun dired-get-size ()
+  (interactive)
+  (let ((files (dired-get-marked-files)))
+    (with-temp-buffer
+      (apply 'call-process "/usr/bin/du" nil t nil "-sch" files)
+      (message "Size of all marked files: %s"
+               (progn
+                 (re-search-backward "\\(^[0-9.,]+[A-Za-z]+\\).*total$")
+                  (match-string 1))))))
+
 (provide 'custom-functions)
