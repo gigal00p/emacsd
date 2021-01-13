f1636e9434192a53231843c908a341f623e6239c
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2021-01-13 11:11:29 +0100

    Group platform specific variables

--- custom-setups/platform-specific-settings.el
+++ custom-setups/platform-specific-settings.el
@@ -10,28 +10,29 @@
     '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
     '(hl-line ((t (:underline nil))))))
 
-
   ((string-equal system-type "darwin")
+   (when (memq window-system '(mac ns x))
+     (setq mac-option-key-is-meta nil)
+     (setq mac-command-key-is-meta t)
+     (setq mac-command-modifier 'meta)
+     (setq mac-option-modifier nil)
+     (require 'keychain-environment)
+     (keychain-refresh-environment))
    (setq system-time-locale "C")
+   (setq dired-use-ls-dired t
+	 insert-directory-program "/usr/local/bin/gls"
+	 dired-listing-switches "-laGh1v --group-directories-first")
    (custom-set-variables
     '(column-number-mode t)
     '(custom-safe-themes
       (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
     '(smooth-scroll-margin 2)))
 
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize)
-    (setq mac-option-key-is-meta nil)
-    (setq mac-command-key-is-meta t)
-    (setq mac-command-modifier 'meta)
-    (setq mac-option-modifier nil)
-    (require 'keychain-environment)
-    (keychain-refresh-environment))
-  
-
   ((string-equal system-type "gnu/linux")
    (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
    (global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
+   (setq dired-use-ls-dired t
+	 dired-listing-switches "-laGh1v --group-directories-first")
    (setq system-time-locale "C")
    (custom-set-variables
     '(column-number-mode t)
@@ -54,14 +55,5 @@
    (custom-set-faces
     '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
     '(hl-line ((t (:underline nil)))))))
-
-(cond (
-       (string= system-type "darwin")
-       (setq dired-use-ls-dired t
-	     insert-directory-program "/usr/local/bin/gls"
-	     dired-listing-switches "-laGh1v --group-directories-first"))
-      ((string= system-type "gnu/linux")
-       (setq dired-use-ls-dired t
-	     dired-listing-switches "-laGh1v --group-directories-first")))
 
 (provide 'platform-specific-settings)
