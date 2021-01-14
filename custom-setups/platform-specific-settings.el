77c4a875e875db4186a3fd2871b5212111c0da8a
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2021-01-14 10:18:14 +0100

    Refactor platform-specific bits to single when stm per platform

--- custom-setups/platform-specific-settings.el
+++ custom-setups/platform-specific-settings.el
@@ -1,59 +1,59 @@
-'(cond
-  ((string-equal system-type "windows-nt")
-   (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
-   (custom-set-variables
-    '(column-number-mode t)
-    '(custom-safe-themes
-      (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
-    '(smooth-scroll-margin 2))
-   (custom-set-faces
-    '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
-    '(hl-line ((t (:underline nil))))))
+(when (string-equal system-type "windows-nt")
+  (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
+  (custom-set-variables
+   '(column-number-mode t)
+   '(custom-safe-themes
+     (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
+   '(smooth-scroll-margin 2))
+  (custom-set-faces
+   '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
+   '(hl-line ((t (:underline nil))))))
 
-  ((string-equal system-type "darwin")
-   (when (memq window-system '(mac ns x))
-     (setq mac-option-key-is-meta nil)
-     (setq mac-command-key-is-meta t)
-     (setq mac-command-modifier 'meta)
-     (setq mac-option-modifier nil)
-     (require 'keychain-environment)
-     (keychain-refresh-environment))
-   (setq system-time-locale "C")
-   (setq dired-use-ls-dired t
-	 insert-directory-program "/usr/local/bin/gls"
-	 dired-listing-switches "-laGh1v --group-directories-first")
-   (custom-set-variables
-    '(column-number-mode t)
-    '(custom-safe-themes
-      (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
-    '(smooth-scroll-margin 2)))
+(when (string-equal system-type "darwin")
+  (setq mac-option-key-is-meta nil)
+  (setq mac-command-key-is-meta t)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier nil)
+  (require 'keychain-environment)
+  (keychain-refresh-environment)
+  (setq system-time-locale "C")
 
-  ((string-equal system-type "gnu/linux")
-   (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
-   (global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
-   (setq dired-use-ls-dired t
-	 dired-listing-switches "-laGh1v --group-directories-first")
-   (setq system-time-locale "C")
-   (custom-set-variables
-    '(column-number-mode t)
-    '(custom-safe-themes
-      (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
-    '(smooth-scroll-margin 2))
-   (custom-set-faces
-    '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
-    '(hl-line ((t (:underline nil))))))
+  (setq dired-use-ls-dired t
+        insert-directory-program "/usr/local/bin/gls"
+        dired-listing-switches "-laGh1v --group-directories-first")
 
-  ((string-equal system-type "berkeley-unix")
-   (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
-   (global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
-   (setq system-time-locale "C")
-   (custom-set-variables
-    '(column-number-mode t)
-    '(custom-safe-themes
-      (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
-    '(smooth-scroll-margin 2))
-   (custom-set-faces
-    '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
-    '(hl-line ((t (:underline nil)))))))
+  (custom-set-variables
+   '(column-number-mode t)
+   '(custom-safe-themes
+     (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
+   '(smooth-scroll-margin 2)))
+
+(when (string-equal system-type "gnu/linux")
+  (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
+  (global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
+  (setq dired-use-ls-dired t
+        dired-listing-switches "-laGh1v --group-directories-first")
+  (setq system-time-locale "C")
+  (custom-set-variables
+   '(column-number-mode t)
+   '(custom-safe-themes
+     (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
+   '(smooth-scroll-margin 2))
+  (custom-set-faces
+   '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
+   '(hl-line ((t (:underline nil))))))
+
+(when (string-equal system-type "berkeley-unix")
+  (setq system-time-locale "C") ; Make sure that the weekdays in timestamps in org mode appear in English
+  (global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
+  (setq system-time-locale "C")
+  (custom-set-variables
+   '(column-number-mode t)
+   '(custom-safe-themes
+     (quote ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
+   '(smooth-scroll-margin 2))
+  (custom-set-faces
+   '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 135 :width normal))))
+   '(hl-line ((t (:underline nil))))))
 
 (provide 'platform-specific-settings)
